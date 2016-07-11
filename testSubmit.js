e234cf0ecee0095b1c28c97432b0b18c6eb81c5d
Brack Carmony <brackcarmony@gmail.com>
2016-07-11 14:28:57 -0600

    This is a thing to test return lines in commit messages
    This is a thing to test more things
    
    And More

--- testSubmit.js
+++ testSubmit.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 
 fs.readFile(__dirname+'/.git/logs/HEAD', 'ascii', function(err, data){
+
   if(err){
     console.log(err);
   }else{
-    console.log(data);
+    var lines = data.split(['\n']);
+    
   }
 })
